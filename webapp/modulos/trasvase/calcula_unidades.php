04b6dffee2adb2bb822fa4e83e94d0faf0798b4e
evesmax <evesmax@gmail.com>
2025-03-03 13:01:41 -0600

    Actualizakardex

--- webapp/modulos/trasvase/calcula_unidades.php
+++ webapp/modulos/trasvase/calcula_unidades.php
@@ -7,7 +7,6 @@
 <head>
 	<title></title>
 	<script language="javascript">
-            
             $(document).ready(function(){
                 $('#i667').bind('blur', function() {  
                         //Agrega nombre de unidad principal
@@ -15,7 +14,7 @@
                             {
                                 var info = datos.split("|");
                                 $('#lbl667').text(info[0]);
-                                $('#lbl667').text(info[1]);
+                                $('#lbl668').text(info[1]);
                                 $('#i668').val(info[2]);
 
                                 if(info[3]==1){
@@ -31,12 +30,32 @@
                         );
                                         
                 });
-                
+                $('#i670').bind('blur', function() {  
+                        //Agrega nombre de unidad principal
+                        $.get('<?php echo $urlapp; ?>',{producto:$('#i664').val(),cantidadp:$('#i670').val(), tipo:1},function(datos)
+                            {
+                                var info = datos.split("|");
+                                $('#lbl670').text(info[0]);
+                                $('#lbl671').text(info[1]);
+                                $('#i671').val(info[2]);
+
+                                if(info[3]==1){
+                                    $('#i670').attr("disabled", true);
+                                    $('#i671').focus();
+                                }
+                                if(info[3]==2){
+                                    $('#i670').removeAttr("disabled");
+                                    $('#i671').focus();
+                                }
+                                
+                            }
+                        );
+                                        
+                });     
                 $('#send').bind('click', function() {  
-                    $('#i669').removeAttr("disabled");              
+                    $('#i669').removeAttr("disabled");  
+                    $('#i671').removeAttr("disabled");              
                 });
-                
-                
             });
 	</script>
 </head>
