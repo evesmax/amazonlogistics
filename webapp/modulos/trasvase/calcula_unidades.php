84531c4475259851b9306f90b92dfe66382be5e3
evesmax <evesmax@gmail.com>
2025-03-04 11:46:43 -0600

    Actualizakardex

--- webapp/modulos/trasvase/calcula_unidades.php
+++ webapp/modulos/trasvase/calcula_unidades.php
@@ -55,6 +55,53 @@
                         );
                                         
                 });     
+                $('#i664').bind('blur', function() {  
+                        //Agrega nombre de unidad principal
+                        $.get('<?php echo $urlapp; ?>',{producto:$('#i664').val(),cantidadp:$('#i667').val(), tipo:1},function(datos)
+                            {
+                                console.log("Producto:", $('#i664').val(), "Cantidad:", $('#i667').val());
+                                alert($('#i664').val());
+                                var info = datos.split("|");
+                                $('#lbl667').text(info[0]);
+                                $('#lbl668').text(info[1]);
+                                $('#i668').val(info[2]);
+
+                                if(info[3]==1){
+                                    $('#i668').attr("disabled", true);
+                                    $('#i669').focus();
+                                }
+                                if(info[3]==2){
+                                    $('#i668').removeAttr("disabled");
+                                    $('#i669').focus();
+                                }
+                                
+                            }
+                        );
+                                        
+                });
+                $('#i669').bind('blur', function() {  
+                        //Agrega nombre de unidad principal
+                        $.get('<?php echo $urlapp; ?>',{producto:$('#i669').val(),cantidadp:$('#i670').val(), tipo:1},function(datos)
+                            {
+                                console.log("Producto:", $('#i669').val(), "Cantidad:", $('#i670').val());
+                                alert($('#i669').val());
+                                var info = datos.split("|");
+                                $('#lbl670').text(info[0]);
+                                $('#lbl671').text(info[1]);
+                                $('#i671').val(info[2]);
+
+                                if(info[3]==1){
+                                    $('#i671').attr("disabled", true);
+                                    $('#i673').focus();
+                                }
+                                if(info[3]==2){
+                                    $('#i671').removeAttr("disabled");
+                                    $('#i673').focus();
+                                }
+                            }
+                        );
+                                        
+                });
                 $('#send').bind('click', function() {  
                     $('#i669').removeAttr("disabled");  
                     $('#i671').removeAttr("disabled");              
