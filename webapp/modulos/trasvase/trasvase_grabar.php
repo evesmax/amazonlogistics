d96bcaf7d9df74216fd5fa16e6f35ece4a9d7f8c
evesmax <evesmax@gmail.com>
2025-03-08 11:18:25 -0600

    Actualizakardex

--- webapp/modulos/trasvase/trasvase_grabar.php
+++ webapp/modulos/trasvase/trasvase_grabar.php
@@ -15,10 +15,7 @@ error_reporting(E_ALL);
             $cantidadmerma2=$_REQUEST["txtcantidadmerma2"];
             $capturista=$_REQUEST["txtcapturista"];
                    
-
-  
 //Recupera valores de consulta
-
         $sQuery = "Select *,CURDATE() fechadia from inventarios_trasvase where idtrasvase".$idtrasvase;
 		$result = $conexion->consultar($sQuery);
 		while($rs = $conexion->siguiente($result)){
@@ -35,48 +32,48 @@ error_reporting(E_ALL);
                 }
 		$conexion->cerrar_consulta($result);
 
-        
+       
 //Afectando Inventario con Documento.
-                        if ($cantidad1>0 {
-                            //Agrega Movimiento Almacen Salida de Producto Origen
-                            $doctoorigen=22;
-                            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$producto,$lote,$estadoproducto,$cantidad1,$cantidad2,$fechadia,$doctoorigen,$idtrsvase,$conexion);
-                        }
+        if ($cantidad1>0 {
+            //Agrega Movimiento Almacen Salida de Producto Origen
+            $doctoorigen=22;
+            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$producto,$lote,$estadoproducto,$cantidad1,$cantidad2,$fechadia,$doctoorigen,$idtrsvase,$conexion);
+        }
 
-                        if ($cantidaddestino1>0 { 
-                            //Agrega Movimiento Almacen Entrada de Nuevo Producto
-                            $doctoorigen=23;
-                            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$productodestino,$lote,$estadoproducto,$cantidaddestino1,$cantidaddestino2,$fechadia,$doctoorigen,$idtrasvase,$conexion);
-                        }
-                        
-                        if ($cantidadpnc1>0 { 
-                            $doctoorigen=23;
-                            $estadopnc=4;
-                            //Agrega Movimiento Almacen Producto No Conforme
-                            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$productodestino,$lote,$estadopnc,$cantidadpnc1,$cantidadpnc2,$fechadia,$doctoorigen,$idtrasvase,$conexion);
-                        }
+        if ($cantidaddestino1>0 { 
+            //Agrega Movimiento Almacen Entrada de Nuevo Producto
+            $doctoorigen=23;
+            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$productodestino,$lote,$estadoproducto,$cantidaddestino1,$cantidaddestino2,$fechadia,$doctoorigen,$idtrasvase,$conexion);
+        }
+        
+        if ($cantidadpnc1>0 { 
+            $doctoorigen=23;
+            $estadopnc=4;
+            //Agrega Movimiento Almacen Producto No Conforme
+            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$productodestino,$lote,$estadopnc,$cantidadpnc1,$cantidadpnc2,$fechadia,$doctoorigen,$idtrasvase,$conexion);
+        }
 
-                        if ($cantidadmerma1>0 { 
-                            //Agrega Movimiento Almacen Producto No Conforme
-                            $doctoorigen=24;
-                           //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$productodestino,$lote,$idestadoproducto,$cantidadmerma1,$cantidadmerma2,$fechadia,$doctoorigen,$idrecepcion,$conexion);
-                        }
+        if ($cantidadmerma1>0 { 
+            //Agrega Movimiento Almacen Producto No Conforme
+            $doctoorigen=24;
+            //$movimientos->agregarmovimiento($tipomovimiento,$fabricante,$marca,$idbodega,$productodestino,$lote,$idestadoproducto,$cantidadmerma1,$cantidadmerma2,$fechadia,$doctoorigen,$idrecepcion,$conexion);
+        }
 
 
-                    //Afecta Cantidades en Logistica_Trasvase
-                        $sqlafecta="UPDATE inventarios_trasvase 
-                                        set cantidaddestinoreal1=$cantidaddestino1,
-                                            cantidaddestinoreal2=$cantidaddestino2,
-                                            cantidadpnc1=$cantidadpnc1,
-                                            cantidadpnc2=$cantidadpnc2,
-                                            cantidadmerma1=$cantidadmerma1,
-                                            cantidadmerma2=$cantidadmerma2,
-                                            idcapturista=$capturista,
-                                            idestadodocumento=2
-                                        Where idtraslado=".$idtrasvase;
-                        echo $sqlafecta;
-                        $conexion->consultar($sqlafecta);
+    //Afecta Cantidades en Logistica_Trasvase
+        $sqlafecta="UPDATE inventarios_trasvase 
+                        set cantidaddestinoreal1=$cantidaddestino1,
+                            cantidaddestinoreal2=$cantidaddestino2,
+                            cantidadpnc1=$cantidadpnc1,
+                            cantidadpnc2=$cantidadpnc2,
+                            cantidadmerma1=$cantidadmerma1,
+                            cantidadmerma2=$cantidadmerma2,
+                            idcapturista=$capturista,
+                            idestadodocumento=2
+                        Where idtraslado=".$idtrasvase;
+        echo $sqlafecta;
+        $conexion->consultar($sqlafecta);
 
-                header("Location: trasvase_imprimir.php?idtrasvase=".$idtrasvase) 
+        header("Location: trasvase_imprimir.php?idtrasvase=".$idtrasvase) 
          
 ?>
