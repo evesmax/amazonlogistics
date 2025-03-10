b5aabe69b6aa3328ea141406d26fbe25a56c8b2c
evesmax <evesmax@gmail.com>
2025-03-09 20:46:40 -0600

    Actualizakardex

--- webapp/modulos/recepciones/recepciondirecta_grabar.php
+++ webapp/modulos/recepciones/recepciondirecta_grabar.php
@@ -1,5 +1,7 @@
 <?php
-	
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 	
 	include("../../netwarelog/catalog/conexionbd.php");
 
@@ -89,7 +91,7 @@
 				$msg=" Faltaron los folios";
 			}
 
-            			if((trim($cartaporte)=="")){
+            if((trim($cartaporte)=="")){
 				$politica=1;
 				$msg=" Falta la Carta Porte";
 			}
