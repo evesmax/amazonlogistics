4e1cbb2305303ea55952474bc4dccdae58e3d17a
evesmax <evesmax@gmail.com>
2025-04-23 22:15:16 -0600

    Actualizakardex

--- webapp/modulos/kardex/kardex.php
+++ webapp/modulos/kardex/kardex.php
@@ -1,11 +1,24 @@
 <?php
-
 ini_set('display_errors', '0');
 
-if (isset($_SESSION['applied_filters']) && is_array($_SESSION['applied_filters'])) {
+include("../../netwarelog/webconfig.php");
+include("../../netwarelog/catalog/conexionbd.php");
 
-    echo "<h2>Filtros Aplicados:</h2>";
-    echo "<ul>";
+set_time_limit($tiempo_timeout);
+//Obtiene usuario
+$usuario=$_SESSION["accelog_idempleado"];
+
+$fechainicial = null;
+$fechafinal = null;
+$idfabricante="NULL";
+$idmarca="NULL";
+$idproducto="NULL";
+$idloteproducto="NULL";
+$idestado="NULL";
+$idbodega="NULL";
+
+
+if (isset($_SESSION['applied_filters']) && is_array($_SESSION['applied_filters'])) {
 
     // 3. Recorre el arreglo de filtros
     foreach ($_SESSION['applied_filters'] as $filtro) {
@@ -18,9 +31,70 @@ if (isset($_SESSION['applied_filters']) && is_array($_SESSION['applied_filters']
 
             // Muestra la información
             echo "<li>" . $etiqueta . ": " . $valor . "</li>";
+            if ($etiqueta == "Del") {
+                $fechainicial = $valor." 00:00:00"; // 2025-02-01 00:00:00
+            }
+            if ($etiqueta == "Al") {
+                $fechainicial = $valor." 23:59:59"; // 2025-02-01 00:00:00
+            }
 
-            // Aquí puedes hacer lo que necesites con $etiqueta y $valor
-            // Por ejemplo, usarlos en consultas SQL, mostrarlos en diferentes formatos, etc.
+            if ($etiqueta == "Propietario") {
+                $sqlAux="Select idfabricante id from operaciones_fabricantes where nombrefabricante like '".$valor."' limit 1";
+                $resultado = $conexion->consultar($sqlAux);
+                while($rs = $conexion->siguiente($resultado)){
+                    $idfabricante=$rs{"id"};
+                }
+                $conexion->cerrar_consulta($resultado);
+            }
+            
+            if ($etiqueta == "Marca") {
+                $sqlAux="Select idmarca id from vista_marcas where nombremarca like '".$valor."' limit 1";
+                $resultado = $conexion->consultar($sqlAux);
+                while($rs = $conexion->siguiente($resultado)){
+                    $idmarca=$rs{"id"};
+                }
+                $conexion->cerrar_consulta($resultado);            
+            }  
+
+            if ($etiqueta == "Producto") {
+
+                    $sqlAux="select idproducto id from inventarios_productos where nombreproducto like '".$valor."' limit 1";
+                    $resultado = $conexion->consultar($sqlAux);
+                    while($rs = $conexion->siguiente($resultado)){
+                        $idproducto=$rs{"id"};
+                    }
+                    $conexion->cerrar_consulta($resultado);
+
+            }
+            if ($etiqueta=="Zafra") {
+
+                    $sqlAux="select idloteproducto id from inventarios_lotes where descripcionlote like '".$valor."' limit 1";
+                    $resultado = $conexion->consultar($sqlAux);
+                    while($rs = $conexion->siguiente($resultado)){
+                        $idloteproducto=$rs{"id"};
+                    }
+                    $conexion->cerrar_consulta($resultado);
+
+            }
+            if ($etiqueta=="Estado Producto") {
+
+                    $sqlAux="select idestadoproducto id from inventarios_estados where descripcionestado like '".$valor."' limit 1";
+                    $resultado = $conexion->consultar($sqlAux);
+                    while($rs = $conexion->siguiente($resultado)){
+                        $idestado=$rs{"id"};
+                    }
+                    $conexion->cerrar_consulta($resultado);
+
+            }    
+            if ($etiqueta=="Nombre Bodega") {
+                    $sqlAux="select idbodega id from operaciones_bodegas where nombrebodega like '".$valor."' limit 1";
+                    $resultado = $conexion->consultar($sqlAux);
+                    while($rs = $conexion->siguiente($resultado)){
+                        $idbodega=$rs{"id"};
+                    }
+                    $conexion->cerrar_consulta($resultado);
+
+            }    
 
         } else {
             // Opcional: Manejar el caso de que un filtro no tenga 'label' o 'value'
@@ -29,8 +103,6 @@ if (isset($_SESSION['applied_filters']) && is_array($_SESSION['applied_filters']
         }
     }
 
-    echo "</ul>";
-
 } else {
     echo "<p>No se encontraron filtros aplicados en la sesión o no es un arreglo.</p>";
     // Puedes añadir un var_dump($_SESSION['applied_filters']); aquí para depurar si es necesario
@@ -38,113 +110,6 @@ if (isset($_SESSION['applied_filters']) && is_array($_SESSION['applied_filters']
 
 
 
-
-
-include("../../netwarelog/webconfig.php");
-include("../../netwarelog/catalog/conexionbd.php");
-
-set_time_limit($tiempo_timeout);
-//Obtiene usuario
-$usuario=$_SESSION["accelog_idempleado"];
-
-
-//Recupero sql real
-$_SESSION["sequel"]=$_SESSION['sql_consulta_original_repologfilters'];
-//Recupera Filtros
-$sqlAux = $_SESSION["sequel"];
-$uw=strpos($sqlAux,'WHERE');
-$ct=strlen($sqlAux);
-$sqlwhere=substr($sqlAux,$uw);
-
-
-// Expresión regular para extraer fechainicial y fechafinal
-if (preg_match('/ik\.fecha between "([^"]+)" And "([^"]+)"/', $sqlwhere, $matches)) {
-    $fechainicial = $matches[1]; // 2025-02-01 00:00:00
-    $fechafinal = $matches[2];   // 2025-02-17 23:59:59
-} else {
-    $fechainicial = null;
-    $fechafinal = null;
-}
-
-// Expresión regular para extraer otros filtros
-$filtros = [];
-if (preg_match_all('/and (\w+\.\w+) like "([^"]*)"/', $sqlwhere, $matches)) {
-    foreach ($matches[1] as $index => $nombreFiltro) {
-        $filtros[$nombreFiltro] = $matches[2][$index];
-    }
-}
-
-$idfabricante="NULL";
-$idmarca="NULL";
-$idproducto="NULL";
-$idloteproducto="NULL";
-$idestado="NULL";
-$idbodega="NULL";
-
-$sqlfiltroslike="";
-foreach ($filtros as $nombre => $valor) {
-    //echo $nombre."=".$valor. "<br>";
-    $sqlAux="";
-    if ($nombre=="of.nombrefabricante") {
-        if ($valor<>"%%"){
-            $sqlAux="Select idfabricante id from operaciones_fabricantes where nombrefabricante like '".$valor."' limit 1";
-            $resultado = $conexion->consultar($sqlAux);
-            while($rs = $conexion->siguiente($resultado)){
-                $idfabricante=$rs{"id"};
-            }
-            $conexion->cerrar_consulta($resultado);
-        }
-    }
-    if ($nombre=="vm.nombremarca") {
-        if ($valor<>"%%"){
-            $sqlAux="Select idmarca id from vista_marcas where nombremarca like '".$valor."' limit 1";
-            $resultado = $conexion->consultar($sqlAux);
-            while($rs = $conexion->siguiente($resultado)){
-                $idmarca=$rs{"id"};
-            }
-            $conexion->cerrar_consulta($resultado);
-        }
-    }
-    if ($nombre=="ip.nombreproducto") {
-        if ($valor<>"%%"){
-            $sqlAux="select idproducto id from inventarios_productos where nombreproducto like '".$valor."' limit 1";
-            $resultado = $conexion->consultar($sqlAux);
-            while($rs = $conexion->siguiente($resultado)){
-                $idproducto=$rs{"id"};
-            }
-            $conexion->cerrar_consulta($resultado);
-        }
-    }
-    if ($nombre=="il.descripcionlote") {
-        if ($valor<>"%%"){
-            $sqlAux="select idloteproducto id from inventarios_lotes where descripcionlote like '".$valor."' limit 1";
-            $resultado = $conexion->consultar($sqlAux);
-            while($rs = $conexion->siguiente($resultado)){
-                $idloteproducto=$rs{"id"};
-            }
-            $conexion->cerrar_consulta($resultado);
-        }
-    }
-    if ($nombre=="ie.descripcionestado") {
-        if ($valor<>"%%"){
-            $sqlAux="select idestadoproducto id from inventarios_estados where descripcionestado like '".$valor."' limit 1";
-            $resultado = $conexion->consultar($sqlAux);
-            while($rs = $conexion->siguiente($resultado)){
-                $idestado=$rs{"id"};
-            }
-            $conexion->cerrar_consulta($resultado);
-        }
-    }    
-    if ($nombre=="ob.nombrebodega") {
-        if ($valor<>"%%"){
-            $sqlAux="select idbodega id from operaciones_bodegas where nombrebodega like '".$valor."' limit 1";
-            $resultado = $conexion->consultar($sqlAux);
-            while($rs = $conexion->siguiente($resultado)){
-                $idbodega=$rs{"id"};
-            }
-            $conexion->cerrar_consulta($resultado);
-        }
-    }    
     //echo $sqlAux."<br>";  
 }
         echo "<br>".$fechainicial." ".$fechafinal." ".$idfabricante." ".$idmarca." ".$idproducto." ".$idloteproducto." ".$idestado." ".$idbodega."<br>";
