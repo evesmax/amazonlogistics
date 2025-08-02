1f6b90eef69934d564836f6f4c6c8e71c0b6563a
evesmax <evesmax@gmail.com>
2025-08-01 23:40:54 -0600

    Actualizakardex

--- webapp/modulos/ordenesentrega/calcula_unidades.php
+++ webapp/modulos/ordenesentrega/calcula_unidades.php
@@ -100,10 +100,20 @@
                         );           
                 });
 
-                $('#i387').bind('onchange', function() {  
-                    $('#i389').val(0);
-                    $('#i390').val(0);
-                    alert("Cambio Producto, se reinician cantidades");
+                $('#i387').on('change', function() {
+                // Obtiene el valor de la opción seleccionada
+                let valorSeleccionado = $(this).val();
+                
+                // Obtiene el texto de la opción seleccionada
+                let textoSeleccionado = $(this).find('option:selected').text();
+
+                // Muestra una alerta con el valor y el texto
+                alert("Seleccionaste la opción con valor: " + valorSeleccionado + " y texto: " + textoSeleccionado);
+                
+                // Aquí puedes poner el resto de tu código
+                // Por ejemplo, reiniciar las cantidades como en tu pregunta anterior.
+                $('#i389').val(0);
+                $('#i390').val(0);
                 });
 
                 $('#send').bind('click', function() {  
