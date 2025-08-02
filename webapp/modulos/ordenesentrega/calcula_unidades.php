d27b06cdbb4aac717de66a6d356369a1343a6f3e
evesmax <evesmax@gmail.com>
2025-08-01 23:22:32 -0600

    Actualizakardex

--- webapp/modulos/ordenesentrega/calcula_unidades.php
+++ webapp/modulos/ordenesentrega/calcula_unidades.php
@@ -19,6 +19,7 @@
                                 $('#lbl389').text(info[0]);
                                 $('#lbl390').text(info[1]);
                                 $('#i390').val(info[2]);
+                                $('send').attr("disabled", false);
 
                                 if(info[3]==1){
                                     $('#i390').attr("disabled", true);
@@ -32,6 +33,7 @@
                                     $('#i654').val(0);
                                     alert("Carta Porte Duplicada");
                                     $('#i654').focus();
+                                    $('send').attr("disabled", false);
                                }
                                //console.log("Duplicada:"+info[4]+" CartaPorte:"+$('#i654').val()+" Transportista"+$('#i653').val());
                             }
@@ -43,6 +45,7 @@
                         $.get('<?php echo $urlapp; ?>',{producto:$('#i387').val(),cantidadp:$('#i389').val(), tipo:1, cartaporte:$('#i654').val(), idtransportista:$('#i653').val()},function(datos)
                             {
                                 //console.log(datos);
+                                $('send').attr("disabled", false);
                                 var info = datos.split("|");
                                 $('#lbl389').text(info[0]);
                                 $('#lbl390').text(info[1]);
@@ -57,6 +60,37 @@
                                     $('#i390').focus();
                                 }
                                if(info[4]>0){
+                                    $('send').attr("disabled", true);
+                                    $('#i654').val(0);
+                                    alert("Carta Porte Duplicada");
+                                    $('#i654').focus();
+                               }
+                               //console.log("Duplicada:"+info[4]+" CartaPorte:"+$('#i654').val()+" Transportista"+$('#i653').val());
+                            }
+                        );           
+                });
+
+                $('#select2-i653-container').bind('blur', function() {  
+                        //Agrega nombre de unidad principal , cartaporte:$('#i654').val(), idtransportista:$('#select2-i653-container').val()
+                        $.get('<?php echo $urlapp; ?>',{producto:$('#i387').val(),cantidadp:$('#i389').val(), tipo:1, cartaporte:$('#i654').val(), idtransportista:$('#i653').val()},function(datos)
+                            {
+                                //console.log(datos);
+                                $('send').attr("disabled", false);
+                                var info = datos.split("|");
+                                $('#lbl389').text(info[0]);
+                                $('#lbl390').text(info[1]);
+                                $('#i390').val(info[2]);
+
+                                if(info[3]==1){
+                                    $('#i390').attr("disabled", true);
+                                    $('#i391').focus();
+                                }
+                                if(info[3]==2){
+                                    $('#i390').removeAttr("disabled");
+                                    $('#i390').focus();
+                                }
+                               if(info[4]>0){
+                                    $('send').attr("disabled", true);
                                     $('#i654').val(0);
                                     alert("Carta Porte Duplicada");
                                     $('#i654').focus();
