cbb973972501017fa0f7c2899ece229b0fd57df4
evesmax <evesmax@gmail.com>
2025-08-29 12:43:12 -0600

    Actualizakardex

--- webapp/modulos/ordenesentrega/calcula_unidades.php
+++ webapp/modulos/ordenesentrega/calcula_unidades.php
@@ -49,7 +49,8 @@
                                 var info = datos.split("|");
                                 $('#lbl389').text(info[0]);
                                 $('#lbl390').text(info[1]);
-                                $('#i390').val(info[2]);
+                                $('#i390').val(parseFloat(info[2]).toFixed(3));
+                                //$('#i390').val(info[2]);
 
                                 if(info[3]==1){
                                     $('#i390').attr("disabled", true);
@@ -79,7 +80,8 @@
                                 var info = datos.split("|");
                                 $('#lbl389').text(info[0]);
                                 $('#lbl390').text(info[1]);
-                                $('#i390').val(info[2]);
+                                $('#i390').text(parseFloat(info[2]).toFixed(3)); 
+                                //$('#i390').val(info[2]);
 
                                 if(info[3]==1){
                                     $('#i390').attr("disabled", true);
