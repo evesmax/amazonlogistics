707c44b4e2d418286cc49d168ae76346164053c1
evesmax <evesmax@gmail.com>
2025-09-23 17:33:06 -0600

    CancelarTrasvase

--- webapp/modulos/trasvase/trasvase_imprimir.php
+++ webapp/modulos/trasvase/trasvase_imprimir.php
@@ -335,7 +335,13 @@
                             //INFORMACION DEL EMISOR
                             $html.="<td width='20%'>";
                                     $html.="<table class='reporte' width='100%'>";
-                                            $html.="<tr class='trencabezado'><td>PROPIETARIO / BODEGA</td></tr>";
+                                            $html.="<tr class='trencabezado'><td>BODEGA ORIGEN</td></tr>";
+                                            $html.="<tr height='55' valign='top'>";
+                                                $html.="<td>";
+                                                    $html.="<br><b>Bodega: ".$bodega."</b><br>".$domiciliobodegaorigen."<br>";
+                                                $html.="</td>";																															
+                                            $html.="</tr>";
+                                            $html.="<tr class='trencabezado'><td>CLIENTE:</td></tr>";
                                             $html.="<tr height='55' valign='top'>";
                                                 $html.="<td>";
                                                     $html.="<b>Propietario: ".$nombreorganizacion."</b>";
@@ -347,7 +353,6 @@
                                                     if($telefonos<>''){
                                                         $html.="<br><strong>TELEFONO:</strong> ".$telefonos;
                                                     }
-                                                    $html.="<br><b>Bodega: ".$bodega."</b><br>".$domiciliobodegaorigen."<br>";
                                                 $html.="</td>";																															
                                             $html.="</tr>";
                                     $html.="</table>";																
@@ -395,7 +400,7 @@
                                                             <td align=left><b>".$cantidad1." Bultos </b></td>
                                                         </tr>";                                               
                         $html.="<tr>
-                                                            <td width=30%>Cantidad a trasvasar:</td>
+                                                            <td width=30%>Cantidad a trasvasar TM:</td>
                                                             <td align=left><b>".$cantidad2." Toneladas </b></td>
                                                         </tr>";
                         $html.="<tr>
@@ -425,7 +430,7 @@
                                                         </tr>";                  
 
                         $html.="<tr>
-                                                            <td width=30%>Cantidad Esperada:</td>
+                                                            <td width=30%>Cantidad Esperada TM:</td>
                                                             <td align=left><b>".$cantidaddestino2." Toneladas </b></td>
                                                         </tr>";  
 						
