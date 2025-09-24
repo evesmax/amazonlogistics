0d9f0ff148a5cece2a6d290c4965eadc9f048d4a
evesmax <evesmax@gmail.com>
2025-09-23 22:51:45 -0600

    CancelarTrasvase

--- webapp/modulos/trasvase/trasvase_imprimir.php
+++ webapp/modulos/trasvase/trasvase_imprimir.php
@@ -89,7 +89,7 @@
                 $carpeta="";
                 $imgtitulo="";
 
-				$tipoimagen="a";
+				$tipoimagen="1";
                 if($tipoimagen=="i"){                        
                         $sqlimagen="select a.logotipo, concat(ifnull(a.rfc,''),' ',a.nombrefabricante) nombre, 
                             concat(a.calle,' ',ifnull(concat('No. ',a.noexterior),''),ifnull(concat('No Int. ',a.nointerior),'')) domicilio,  
