ff008caaca82d78bde12b158b3e81643c3162f97
evesmax <evesmax@gmail.com>
2025-12-26 12:26:42 -0600

    CancelarTrasvase

--- webapp/modulos/recepciones/recepciondirecta.php
+++ webapp/modulos/recepciones/recepciondirecta.php
@@ -759,7 +759,6 @@
             $sqlest = "Select idestadoproducto,descripcionestado from inventarios_estados where espnc=-1";
             $result = $conexion->consultar($sqlest);
             
-            $cmbestados= "<option value='-1'>--Seleccionar--</option>";
             while ($rs = $conexion->siguiente($result)) {
                 $cmbestados.= "<option value='" . $rs{"idestadoproducto"} . "'>" . $rs{"descripcionestado"} . "</option>";
             }
