fccde18f4d60e240dce61dac141b79b31ebb54fa
evesmax <evesmax@gmail.com>
2026-01-12 20:02:41 -0600

    CancelarTrasvase

---
+++ webapp/modulos/envios/foliorecepcion.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Deshabilitar Campo</title>
+
+    <script language="javascript">
+        $(document).ready(function(){
+            // 2. Usar .prop() para deshabilitar el campo con ID 'i671'
+            $('#i704').prop('disabled', true);
+        });
+    </script>
+</head>
+<body>
+</body>
+</html>
