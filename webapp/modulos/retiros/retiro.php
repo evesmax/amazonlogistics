5a14e6c6090431826101c01b7ddece54048cbc89
evesmax <evesmax@gmail.com>
2026-02-28 12:52:43 -0600

    CancelarTrasvase

--- webapp/modulos/retiros/retiro.php
+++ webapp/modulos/retiros/retiro.php
@@ -177,7 +177,7 @@ $htmlpoliticas="
         //Verifica Inventario
         $inventario=0;
         $sqlinv="call generaExistenciasInventario('".$fecha_final."',".$idfabricante.",".$idmarca.",".$idbodega.",".$idproducto.",".$idloteproducto.",".$idestadoproducto.",".$idempleado.")";
-        //echo "<br> Consulta Inventario".$sqlinv."<br>";
+        echo "<br> Consulta Inventario".$sqlinv."<br>";
         $conexion->consultar($sqlinv);
         $sqlinv="Select inventarioinicial from inventarios_existencias where idempleado=".$idempleado;
         //echo "<br> Consulta Inventario".$sqlinv."<br>";
